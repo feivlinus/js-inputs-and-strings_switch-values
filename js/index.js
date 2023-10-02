05e38a1a5a7ad345643e03ccc063cf1143cc9cd7
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-02 15:05:37 +0200

    Challenge completet

--- js/index.js
+++ js/index.js
@@ -14,13 +14,24 @@ Hint: the HTML entity for the switch button is: &updownarrow
 */
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  const inputOneValue = firstInput.value;
+  const inputTwoValue = secondInput.value;
+  firstInput.value = inputTwoValue;
+  secondInput.value = inputOneValue;
 });
